bdfee3db94fa59cbed87a360c5a14e275c74da36
Ramananpydev <ramananpydev@gmail.com>
2026-01-30 16:23:47 +0530

    Fix routing for local and GitHub Pages deployment

--- frontend/Hub/src/main.jsx
+++ frontend/Hub/src/main.jsx
@@ -1,8 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 
+
+
+const basename = import.meta.env.PROD ? "/sweet-savory" : "/";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename="/sweet-savory">
