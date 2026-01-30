940e6080864e3cad73a85a5757a6e25bba7b2f59
Ramananpydev <ramananpydev@gmail.com>
2026-01-30 11:35:35 +0530

    'fixpath'

--- frontend/Hub/src/main.jsx
+++ frontend/Hub/src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter basename="/sweet-savory">
     <App />
+    </BrowserRouter>
   </StrictMode>,
 )
