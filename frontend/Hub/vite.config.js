2b424792a08d773a151c014e7cc4ff5ebb2c1047
Ramananpydev <ramananpydev@gmail.com>
2026-01-30 16:42:15 +0530

    Fix routing for local and GitHub Pages deployment

--- frontend/Hub/vite.config.js
+++ frontend/Hub/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/sweet-savory/",
+  base: "/ecommerce-react--django_restapi/",
 })
